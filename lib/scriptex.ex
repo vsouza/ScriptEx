a3adfb08a82ed692c9f36118ea7738dab621f127
Vinicius Souza <hi@vsouza.com>
2018-07-16 16:08:20 -0300

    Receive param to call module

--- lib/scriptex.ex
+++ lib/scriptex.ex
@@ -1,2 +1,10 @@
 defmodule Scriptex do
+  def main(_args) do
+    execute_command(_args)
+  end
+
+  defp execute_command(["weather" | params]) do
+    Enum.join(params, " ")
+    |> Weather.info
+  end
 end
